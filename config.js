26108b4265811cdca501c66a4653ffa2eb59624c
DINUWH-MD <dinethnethruvan772@gmail.com>
2024-12-25 11:38:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
-    OWNER_NUMBER: process.env.OWNER_NUMBER || '',    // Enter Your Owner Number
+    SESSION_ID: process.env.SESSION_ID || 'NQxnSKqQ#1dIsEhh3PpSOX_TjuX1w8ZK8mNrxa8skrM6nnzsaHD0', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || 'mongodb://mongo:aHCBTsUhzVIvadJNEBXYgzCePbwwkAhj@junction.proxy.rlwy.net:51068',// Enter Your MongoDB URL
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '94764582504',    // Enter Your Owner Number
 };
